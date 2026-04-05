6a1f549f40b4786fa1857c5ff55a2d09ac897e76
Jakub Raczynski <myszsoda@gmail.com>
2026-04-05 16:20:41 +0200

    Revert "Further reduce settlements in range scan operations"
    This reverts commit ce005439fec1677ed27eb6bd34e55da0d0b0cab1.
    
    After considereation and testing, for big ranges and longer saves,
    having such low limit creates trend towards using caravans mostly,
    due to amount of friendly settlements in proximity.

--- Source/RimWar/Planet/WorldUtility.cs
+++ Source/RimWar/Planet/WorldUtility.cs
@@ -21,7 +21,7 @@ namespace RimWar.Planet
         private static WorldComponent_PowerTracker wcpt = null;
 
         // Do not scan literally everything, put some arbitrary limit
-        private const int maxObjectsPerScan = 5;
+        private const int maxObjectsPerScan = 20;
 
         public static WorldComponent_PowerTracker Get_WCPT()
         {
